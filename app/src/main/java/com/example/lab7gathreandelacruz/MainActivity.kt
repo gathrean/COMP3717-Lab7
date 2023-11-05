c08dadf6d282e71897968cd61b0d9adfcb44ecc4
Gathrean Dela Cruz <gathrean@icloud.com>
2023-11-05 10:40:44 -0800

    Implemented a Character data class to match the string array names into their respective character images.

--- app/src/main/java/com/example/lab7gathreandelacruz/MainActivity.kt
+++ app/src/main/java/com/example/lab7gathreandelacruz/MainActivity.kt
@@ -8,9 +8,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab7gathreandelacruz.ui.theme.Lab7GathreanDelaCruzTheme
+
+// Character data class for the list of characters
+data class Character(val name: String, val imageId: Int)
 
 /**
  * Gathrean Dela Cruz, A01167248
@@ -29,12 +35,29 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
-            MainContent()
+            val characterNames = stringArrayResource(id = R.array.characters)
+
+            val characterList = listOf(
+                Character(characterNames[0], R.drawable.batbron),
+                Character(characterNames[1], R.drawable.beebron),
+                Character(characterNames[2], R.drawable.cheesedrake),
+                Character(characterNames[3], R.drawable.fortnitebron),
+                Character(characterNames[4], R.drawable.frogbron),
+                Character(characterNames[5], R.drawable.sillykittybron),
+                Character(characterNames[6], R.drawable.spongebron),
+                Character(characterNames[7], R.drawable.unicornbron),
+            )
+            MainContent(characterList)
         }
     }
 }
 
 @Composable
-fun MainContent() {
+fun MainContent(characterList: List<Character>) {
+
+    val characterStateList = remember {
+        mutableStateListOf<Character>()
+    }
+
     Text(text = "Hello World!")
 }
